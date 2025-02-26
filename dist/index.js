3e740b1bc9595f27e3240e75aef21778b99527d4
Lucca-Vilaca <lucca.vilaca@gmail.com>
2025-02-25 22:36:56 -0300

    Exercícios concluídos

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,22 @@
-let mensagem = "Olá, TypeScript!";
-console.log(mensagem);
+//Exercício 1
+let valores = [10, 15, 20];
+let somaValores = valores.reduce((acc, val) => acc + val, 0);
+console.log("Soma:", somaValores);
+//Exercício 2
+function mensagem(nome) {
+    return `Olá, ${nome}! Como você está ?`;
+}
+console.log(mensagem("Lucca"));
+//Exercício 3
+class Carro {
+    constructor(marca, modelo, ano) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+    }
+    detalhes() {
+        return `${this.marca} ${this.modelo}, Ano: ${this.ano}`;
+    }
+}
+const carro = new Carro("Ford", "Mustang", 2020);
+console.log(carro.detalhes());
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,20 @@
-let mensagem: string = "Olá, TypeScript!";
-console.log(mensagem);
+//Exercício 1
+let valores: number[] = [10, 15, 20];
+let somaValores: number = valores.reduce((acc, val) => acc + val, 0);
+console.log("Soma:", somaValores);
+
+//Exercício 2
+function mensagem(nome: string): string {
+    return `Olá, ${nome}! Como você está ?`;
+}
+console.log(mensagem("Lucca"));
+
+//Exercício 3
+class Carro {
+    constructor(public marca: string, public modelo: string, public ano: number) {}
+    detalhes(): string {
+        return `${this.marca} ${this.modelo}, Ano: ${this.ano}`;
+    }
+}
+const carro = new Carro("Ford", "Mustang", 2020);
+console.log(carro.detalhes())
