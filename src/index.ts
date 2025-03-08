bb59c9b2f2f74f088169d4b4e6f60c983f9d3dda
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-07 22:32:24 -0300

    Fazendo exercícios usando type e interfaces

--- src/index.ts
+++ src/index.ts
@@ -18,3 +18,32 @@ class Carro {
 }
 const carro = new Carro("Ford", "Mustang", 2020);
 console.log(carro.detalhes())
+
+//Aula 07/03
+
+//Exercício 1
+interface Car {
+    marca: string;
+    modelo: string;
+    ano: number;
+    motor?:string;
+}
+const meuCarro: Car = {
+    marca: "Volkswagen",
+    modelo: "gol",
+    ano: 2016
+};
+
+//Exercício 2
+
+interface Multiplicacao {
+    num1: number,
+    num2: number
+};
+const operacao: Multiplicacao = {
+    num1: 10,
+    num2: 10
+};
+function multiplica (num1, num2){
+    console.log(`O resultado da multiplicação é: ${num1 * num2}`)
+}
